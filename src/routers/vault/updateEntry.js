dd53d77f1d562813db175253bc628f1f9e2bfb61
asadCSE <asadcse247@gmail.com>
2020-11-16 16:33:35 +0600

    update update

--- src/routers/vault/updateEntry.js
+++ src/routers/vault/updateEntry.js
@@ -3,15 +3,15 @@ const jwt = require('jsonwebtoken')
 const User = require('../../models/UserModel')
 const updateEntry = new express.Router()
 
-updateEntry.post('/', async (req, res) => {
-    if(req.headers.bearer && req.body.updateId && req.body.siteNote.length < 101){
+updateEntry.put('/', async (req, res) => {
+        if(req.headers.bearer && req.body.updateId && req.body.siteNote.length < 101 && req.body.sitePass){
         jwt.verify(req.headers.bearer, process.env.JWTSECRET, async (err, data) => {
             if(err) {return res.status(406).send({Error: "not acceptable!"})}
             try {
                 const user = await User.findOne({_id: data.data})
                 if(!user) {return res.status(404).send({Error: "user not found!"})}
                 const entries = user.userVault.filter((entry) => entry._id == req.body.updateId ? false : true)
-                const crypt = jwt.verify(req.body.sitePass, process.env.JWTSECRET)
+                const crypt = jwt.sign(req.body.sitePass, process.env.JWTSECRET)
                 entries.push({
                     site: req.body.site,
                     siteLogin: req.body.siteLogin,
@@ -19,7 +19,7 @@ updateEntry.post('/', async (req, res) => {
                     siteNote: req.body.siteNote
                 })
                 await User.findOneAndUpdate({_id: data.data}, {userVault: entries})
-                res.send()
+                res.send({Success: "entry updated"})
             }catch(e) {
                 res.status(500).send({Error: "server error!"})
             }
