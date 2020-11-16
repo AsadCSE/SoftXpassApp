d3c965e5f2c5b5ce64e959d9474956c462ce5a00
asadCSE <asadcse247@gmail.com>
2020-11-16 12:08:20 +0600

    update user git

--- src/routers/user/readUser.js
+++ src/routers/user/readUser.js
@@ -14,4 +14,16 @@ readUser.get('/', async (req, res) => {
     res.status(400).send({Error: "bad request!"})
 })
 
+readUser.get('/check', async (req, res) => {
+    if(!(req.headers.userphone && req.headers.secret == 'V]W9EphbD/77>V-"^q')){return res.status(400).send({Error: "bad request!"})}
+    try{
+        const checkUser = await User.findOne({userPhone: req.headers.userphone})
+        if(!checkUser){return res.status(400).send({Error: "no user found"})}
+        const token = jwt.sign({ data: checkUser.userPhone }, process.env.JWTSECRET)
+        res.send({token})
+    }catch(e){
+        res.status(500).send({Error: "server error"})
+    }
+})
+
 module.exports = readUser
--- src/routers/user/updateUser.js
+++ src/routers/user/updateUser.js
@@ -1,4 +1,23 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../../models/UserModel')
+const jwt = require('jsonwebtoken')
 const updateUser = new express.Router()
+
+updateUser.put('/', async (req, res) => {
+    if(!(req.headers.token && req.body.userPass)){return res.status(400).send({Error: "bad request"})}
+    try{
+        jwt.verify(req.headers.token, process.env.JWTSECRET, async (err, data) => {
+            if(err) {return res.status(400).send({Error: "bad request"})}
+            const user = await User.findOne({userPhone: data.data})
+            if(!user){return res.send({Error: "bad request"})}
+            const passCrypt = await bcrypt.hash(req.body.userPass, 3)
+            await User.findOneAndUpdate({userPhone: user.userPhone}, {userPass: passCrypt}, {new:true})
+            res.send({Success: "password updated"})
+        })
+    }catch(e){
+        res.status(500).send({Error: "server error"})
+    }
+})
 
 module.exports = updateUser
